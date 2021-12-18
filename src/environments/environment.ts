16ee55d996a58041d2c3e4ec46ed01da03d24f15
Your Name <you@example.com>
2021-12-18 22:03:46 +0700

    smart-coffee

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'xxx',
-    authDomain: 'angular-10-crud-db.firebaseapp.com',
-    databaseURL: 'https://angular-10-crud-db.firebaseio.com',
-    projectId: 'angular-10-crud-db',
-    storageBucket: 'angular-10-crud-db.appspot.com',
-    messagingSenderId: 'xxx',
-    appId: '1:xxx:web:xxx'
+    apiKey: "AIzaSyCyIXbU9EBwXIiA1wC2MyEmXkSMPrSpQN8",
+    authDomain: "smartcoffee-1c76b.firebaseapp.com",
+    databaseURL: "https://smartcoffee-1c76b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smartcoffee-1c76b",
+    storageBucket: "smartcoffee-1c76b.appspot.com",
+    messagingSenderId: "298982192689",
+    appId: "1:298982192689:web:4f29fd15a35463e9bd0710",
+    measurementId: "G-1WMDR9PM35"
   }
 };
